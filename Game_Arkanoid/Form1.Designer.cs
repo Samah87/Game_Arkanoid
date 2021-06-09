c6430f06c57dc5d480da43a137401b6e5e7281e9
samah maher <lilasbluma@gmail.com>
2021-06-09 21:53:37 +0200

    Add project files.

---
+++ Game_Arkanoid/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace Game_Arkanoid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.TxtPunkte = new System.Windows.Forms.Label();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.HightBox = new System.Windows.Forms.PictureBox();
+            this.HightPunkte = new System.Windows.Forms.Label();
+            this.TxtGewonnen = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HightBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Player
+            // 
+            this.Player.BackColor = System.Drawing.Color.Transparent;
+            this.Player.BackgroundImage = global::Game_Arkanoid.Properties.Resources.akanoid_player;
+            this.Player.Location = new System.Drawing.Point(357, 519);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(126, 30);
+            this.Player.TabIndex = 0;
+            this.Player.TabStop = false;
+            // 
+            // Ball
+            // 
+            this.Ball.BackColor = System.Drawing.Color.Transparent;
+            this.Ball.BackgroundImage = global::Game_Arkanoid.Properties.Resources.akanoid_ball;
+            this.Ball.Location = new System.Drawing.Point(409, 427);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(16, 16);
+            this.Ball.TabIndex = 1;
+            this.Ball.TabStop = false;
+            // 
+            // TxtPunkte
+            // 
+            this.TxtPunkte.AutoSize = true;
+            this.TxtPunkte.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtPunkte.Location = new System.Drawing.Point(13, 11);
+            this.TxtPunkte.Name = "TxtPunkte";
+            this.TxtPunkte.Size = new System.Drawing.Size(88, 24);
+            this.TxtPunkte.TabIndex = 2;
+            this.TxtPunkte.Text = "Punkte: 0";
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.GameTimer_Tick);
+            // 
+            // HightBox
+            // 
+            this.HightBox.BackColor = System.Drawing.Color.Transparent;
+            this.HightBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("HightBox.BackgroundImage")));
+            this.HightBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.HightBox.Location = new System.Drawing.Point(135, 179);
+            this.HightBox.Name = "HightBox";
+            this.HightBox.Size = new System.Drawing.Size(615, 202);
+            this.HightBox.TabIndex = 3;
+            this.HightBox.TabStop = false;
+            this.HightBox.Visible = false;
+            // 
+            // HightPunkte
+            // 
+            this.HightPunkte.AutoSize = true;
+            this.HightPunkte.BackColor = System.Drawing.Color.Aqua;
+            this.HightPunkte.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HightPunkte.ForeColor = System.Drawing.Color.OrangeRed;
+            this.HightPunkte.Location = new System.Drawing.Point(350, 274);
+            this.HightPunkte.Name = "HightPunkte";
+            this.HightPunkte.Size = new System.Drawing.Size(182, 39);
+            this.HightPunkte.TabIndex = 4;
+            this.HightPunkte.Text = "Punkte: 80";
+            this.HightPunkte.Visible = false;
+            // 
+            // TxtGewonnen
+            // 
+            this.TxtGewonnen.AutoSize = true;
+            this.TxtGewonnen.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtGewonnen.ForeColor = System.Drawing.Color.Snow;
+            this.TxtGewonnen.Location = new System.Drawing.Point(368, 393);
+            this.TxtGewonnen.Name = "TxtGewonnen";
+            this.TxtGewonnen.Size = new System.Drawing.Size(153, 31);
+            this.TxtGewonnen.TabIndex = 5;
+            this.TxtGewonnen.Text = "Gewonnen";
+            this.TxtGewonnen.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.TxtGewonnen);
+            this.Controls.Add(this.HightPunkte);
+            this.Controls.Add(this.HightBox);
+            this.Controls.Add(this.TxtPunkte);
+            this.Controls.Add(this.Ball);
+            this.Controls.Add(this.Player);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.MaximumSize = new System.Drawing.Size(900, 600);
+            this.MinimumSize = new System.Drawing.Size(900, 600);
+            this.Name = "Form1";
+            this.Text = "Arkanoid";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.IsKeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.IsKeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HightBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.Label TxtPunkte;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.PictureBox HightBox;
+        private System.Windows.Forms.Label HightPunkte;
+        private System.Windows.Forms.Label TxtGewonnen;
+    }
+}
+
---
+++ Game_Arkanoid/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game_Arkanoid
+{
+    public partial class Form1 : Form
+    {
+
+        int Punkte = 0;
+        int BallX;
+        int BallY;
+        int PlayerSpeed;
+        bool goLeft, goRight, isGameover;
+
+        PictureBox[] blockArray;
+
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            SetupGame();
+        }
+
+        private void SetupGame()
+        {
+            isGameover = false;
+            Punkte = 0;
+            BallX = 5;
+            BallY = 5;
+            PlayerSpeed = 12;
+
+            TxtPunkte.Text = "Punkte: " + Punkte;
+            Ball.Left = 410;
+            Ball.Top = 330;
+
+            Player.Left = 375;
+            Player.Top = 514;
+
+            blockArray = new PictureBox[40];
+
+            int a = 0;
+            int top = 90;
+            int links = 140;
+
+            for(int t = 0; t < blockArray.Length; t++)
+            {
+                blockArray[t] = new PictureBox
+                {
+                    Width = 54,
+                    Height = 22,
+                    //BackColor = Color.FromArgb(rnd.Next(255), rnd.Next(255), rnd.Next(255)),
+                    BackColor = Color.Transparent,
+                    BackgroundImage = Properties.Resources.akanoid_block,
+                    Tag = "blocks"
+                };
+
+                if(a == 10)
+                {
+                    top += 24;
+                    links = 140;
+                    a = 0;
+                }
+
+                if(a < 10)
+                {
+                    a++;
+                    blockArray[t].Left = links;
+                    blockArray[t].Top = top;
+                    this.Controls.Add(blockArray[t]);
+                    links += 60;
+                }
+
+            }
+
+            GameTimer.Start();
+
+        }
+
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            TxtPunkte.Text = "Punkte: " + Punkte;
+            if(goLeft == true && Player.Left > 0)
+            {
+                Player.Left -= PlayerSpeed;
+            }
+
+            if(goRight == true && Player.Left < this.ClientSize.Width - Player.Width)
+            {
+                Player.Left += PlayerSpeed;
+            }
+
+            Ball.Left += BallX;
+            Ball.Top += BallY;
+
+            if(Ball.Left <0 || Ball.Left > this.ClientSize.Width - Ball.Width)
+            {
+                BallX = -BallX;
+            }
+
+            if(Ball.Top < 0)
+            {
+                BallY = -BallY;
+            }
+
+            if (Ball.Bounds.IntersectsWith(Player.Bounds))
+            {
+                Ball.Top = 503;
+
+                //BallY = -BallY;
+
+                BallY = rnd.Next(5, 12) * -1;
+
+                if(BallX < 0)
+                {
+                    BallX = rnd.Next(5, 12) * -1;
+                }
+                else
+                {
+                    BallX = rnd.Next(5, 12);
+                }
+            }
+
+            foreach(Control x in this.Controls)
+            {
+                if(x is PictureBox && (string)x.Tag == "blocks")
+                {
+                    if (Ball.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        Punkte++;
+                        BallY = -BallY;
+
+                        this.Controls.Remove(x);
+                    }
+                }
+            }
+
+            if(Punkte == blockArray.Length)
+            {
+                GameOver("G");
+            }
+
+
+            if(Ball.Top > this.ClientSize.Height - Ball.Height)
+            {
+                GameOver("V");
+            }
+        }
+
+
+        private void GameOver(string Zeichen)
+        {
+            GameTimer.Stop();
+            isGameover = true;
+            HightBox.Visible = true;
+            HightPunkte.Text = "Punkte: " + Punkte;
+            HightPunkte.Visible = true;
+            HightPunkte.BringToFront();
+
+            if(Zeichen == "G")
+            {
+                TxtGewonnen.Text = "Gewonnen";
+                TxtGewonnen.Visible = true;
+            }
+            else
+            {
+                TxtGewonnen.Text = "Verloren";
+                TxtGewonnen.Visible = true;
+            }
+        }
+
+
+        private void IsKeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+            if(e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+        }
+
+        private void IsKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+
+            if(e.KeyCode == Keys.Enter && isGameover == true)
+            {
+                ResetBlock();
+                SetupGame();
+            }
+        }
+
+        private void ResetBlock()
+        {
+            foreach(PictureBox x in blockArray)
+            {
+                this.Controls.Remove(x);
+            }
+
+            HightBox.Visible = false;
+            HightPunkte.Visible = false;
+            TxtGewonnen.Visible = false;
+        }
+    }
+}
